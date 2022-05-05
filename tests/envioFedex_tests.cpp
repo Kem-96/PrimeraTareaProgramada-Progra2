f456da5524aa4bddfe322ac32f545daddcde4fdd
Kem-96 <kemblyj_1796@gmail.com>
2022-05-05 12:28:20 -0600

    implementacion de los tests en envioFedex

--- tests/envioFedex_tests.cpp
+++ tests/envioFedex_tests.cpp
@@ -4,8 +4,47 @@
 
 namespace
 {
+    //Test para el constructor
+    TEST(EnvioFedex_ObtenerCantKilometros_Test, Test_0__Retorna_true)
+    {
+        /// AAA
 
-    TEST(EnvioFedex_CalculoEnvio_Test, Test_0_Distancia_Mayor500km_Retorna_50)
+        // Arrange - configurar el escenario
+        float km = 500.00;
+        float peso = 5.00;
+        EnvioFedex* envio1 = new EnvioFedex(km, peso);
+        
+        delete envio1;
+
+        // Act - ejecute la operación
+        float actual = envio1->ObtenerCantKilometros();
+        float esperada = 500.00;
+
+        // Assert - valide los resultados
+       ASSERT_FLOAT_EQ(esperada, actual);
+    }
+
+    TEST(EnvioFedex_ObtenerPesoPaquete_Test, Test_1__Retorna_true)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+        float km = 500.00;
+        float peso = 5.25;
+        EnvioFedex* envio1 = new EnvioFedex(km, peso);
+        
+        delete envio1;
+
+        // Act - ejecute la operación
+        float actual = envio1->ObtenerPesoPaquete();
+        float esperada = 5.25;
+
+        // Assert - valide los resultados
+       ASSERT_FLOAT_EQ(esperada, actual);
+    }
+
+    //Test para el calculo de los envios 
+    TEST(EnvioFedex_CalculoEnvio_Test, Test_2_Distancia_Mayor500km_Retorna_50)
     {
         /// AAA
 
@@ -23,5 +62,63 @@ namespace
         // Assert - valide los resultados
         EXPECT_FLOAT_EQ(esperada, actual);
     }
+    
+    TEST(EnvioFedex_CalculoEnvio_Test, Test_3_Distancia_Menor500km_Retorna_35)
+    {
+        /// AAA
 
+        // Arrange - configurar el escenario
+        float km = 200.00;
+        float peso = 5.00;
+        EnvioFedex* envio1 = new EnvioFedex(km, peso);
+        
+        delete envio1;
+
+        // Act - ejecute la operación
+        float actual = envio1->CalculoEnvio();
+        float esperada = 35.00;
+
+        // Assert - valide los resultados
+        EXPECT_FLOAT_EQ(esperada, actual);
+    }
+
+    TEST(EnvioFedex_CalculoEnvio_Test, Test_4_Peso_Mayor10kg_Retorna_45)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+        float km = 300.00;
+        float peso = 15.00;
+        EnvioFedex* envio1 = new EnvioFedex(km, peso);
+        
+        delete envio1;
+
+        // Act - ejecute la operación
+        float actual = envio1->CalculoEnvio();
+        float esperada = 45.00;
+
+        // Assert - valide los resultados
+        EXPECT_FLOAT_EQ(esperada, actual);
+    }
+
+    TEST(EnvioFedex_CalculoEnvio_Test, Test_5_Peso_Menor10kg_Retorna_35)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+        float km = 100.00;
+        float peso = 5.15;
+        EnvioFedex* envio1 = new EnvioFedex(km, peso);
+        
+        delete envio1;
+
+        // Act - ejecute la operación
+        float actual = envio1->CalculoEnvio();
+        float esperada = 35.00;
+
+        // Assert - valide los resultados
+        EXPECT_FLOAT_EQ(esperada, actual);
+    }
+
+    
 }
--- tests/envioServicioPostal_tests.cpp
+++ tests/envioServicioPostal_tests.cpp
@@ -3,78 +3,154 @@
 
 namespace
 {
-    TEST(CalculoEnvioFedex_Test, Test_0_Retorna_50)
+
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_0_PrimeraClase_PesoEntre0y3_Retorna_15)
     {
+        
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioServicioPostal envio;
+        float km = 50.00;
+        int peso = 3;
+        int clase = 0; // 0 equivale a primera clase
+        EnvioServicioPostal* envio1 = new EnvioServicioPostal(clase, peso, km);
+        
+        delete envio1;
 
         // Act - ejecute la operación
-        int actual = envio.CalculoEnvioServicioPostal();
-        int esperada = 50;
+        float actual = envio1->CalculoEnvio();
+        float esperada = 15.00;
 
         // Assert - valide los resultados
-        EXPECT_EQ(esperada, actual);
+        EXPECT_FLOAT_EQ(esperada, actual);
     }
 
-    TEST(CalculoEnvioFedex_Test, Test_1_Retorna_1)
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_1_PrimeraClase_Peso_Entre4y8_Retorna_31)
     {
+        
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioServicioPostal envio;
+        float km = 70.00;
+        int peso = 6;
+        int clase = 0; // 0 equivale a primera clase
+        EnvioServicioPostal* envio1 = new EnvioServicioPostal(clase, peso, km);
+        
+        delete envio1;
 
         // Act - ejecute la operación
-        int actual = envio.CalculoEnvioServicioPostal();
-        int esperada = 50;
+        float actual = envio1->CalculoEnvio();
+        float esperada = 31.5;
 
         // Assert - valide los resultados
-        EXPECT_EQ(esperada, actual);
+        EXPECT_FLOAT_EQ(esperada, actual);
     }
 
-    TEST(CalculoEnvioFedex_Test, Test_2_Retorna_2)
+
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_2_PrimeraClase_Peso_mayor9_Retorna_24)
     {
-         /// AAA
+        
+        /// AAA
 
         // Arrange - configurar el escenario
-        EnvioServicioPostal envio;
+        float km = 40.00;
+        int peso = 12;
+        int clase = 0; // 0 equivale a primera clase
+        EnvioServicioPostal* envio1 = new EnvioServicioPostal(clase, peso, km);
+        
+        delete envio1;
 
         // Act - ejecute la operación
-        int actual = envio.CalculoEnvioServicioPostal();
-        int esperada = 50;
+        float actual = envio1->CalculoEnvio();
+        float esperada = 24.00;
 
         // Assert - valide los resultados
-        EXPECT_EQ(esperada, actual);
+        EXPECT_FLOAT_EQ(esperada, actual);
     }
 
-    TEST(CalculoEnvioFedex_Test, Test_3_Retorna_6)
+
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_3_SegundaClase_Peso_Entre0y3_Retorna_081)
     {
-         /// AAA
+        
+        /// AAA
 
         // Arrange - configurar el escenario
-        EnvioServicioPostal envio;
+        float km = 30.00;
+        int peso = 2;
+        int clase = 1; // 1 equivale a Segunda clase
+        EnvioServicioPostal* envio1 = new EnvioServicioPostal(clase, peso, km);
+        
+        delete envio1;
 
         // Act - ejecute la operación
-        int actual = envio.CalculoEnvioServicioPostal();
-        int esperada = 50;
+        float actual = envio1->CalculoEnvio();
+        float esperada = 0.84;
 
         // Assert - valide los resultados
-        EXPECT_EQ(esperada, actual);
+        EXPECT_FLOAT_EQ(esperada, actual);
     }
 
-    TEST(CalculoEnvioFedex_Test, Test_4_Retorna_24)
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_4_SegundaClase_Peso_Entre4y8_Retorna_477)
     {
-         /// AAA
+        
+        /// AAA
 
         // Arrange - configurar el escenario
-        EnvioServicioPostal envio;
+        float km = 90.00;
+        int peso = 5;
+        int clase = 1; // 1 equivale a Segunda clase
+        EnvioServicioPostal* envio1 = new EnvioServicioPostal(clase, peso, km);
+        
+        delete envio1;
 
         // Act - ejecute la operación
-        int actual = envio.CalculoEnvioServicioPostal();
-        int esperada = 50;
+        float actual = envio1->CalculoEnvio();
+        float esperada = 4.77;
 
         // Assert - valide los resultados
-        EXPECT_EQ(esperada, actual);
+        EXPECT_FLOAT_EQ(esperada, actual);
     }
+
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_5_SegundaClase_Peso_Mayor9_Retorna_1125)
+    {
+        
+        /// AAA
+
+        // Arrange - configurar el escenario
+        float km = 150.00;
+        int peso = 15;
+        int clase = 1; // 1 equivale a Segunda clase
+        EnvioServicioPostal* envio1 = new EnvioServicioPostal(clase, peso, km);
+        
+        delete envio1;
+
+        // Act - ejecute la operación
+        float actual = envio1->CalculoEnvio();
+        float esperada = 11.25;
+
+        // Assert - valide los resultados
+        EXPECT_FLOAT_EQ(esperada, actual);
+    }
+
+    TEST(EnvioServicioPostal_CalculoEnvio_Test, Test_6_TerceraClase_Peso_Entre4y8_Retorna_096)
+    {
+        
+        /// AAA
+
+        // Arrange - configurar el escenario
+        float km = 80.00;
+        int peso = 7;
+        int clase = 2; // 2 equivale a Tercera clase
+        EnvioServicioPostal* envio1 = new EnvioServicioPostal(clase, peso, km);
+        
+        delete envio1;
+
+        // Act - ejecute la operación
+        float actual = envio1->CalculoEnvio();
+        float esperada = 0.96;
+
+        // Assert - valide los resultados
+        EXPECT_FLOAT_EQ(esperada, actual);
+    }
+
 }
